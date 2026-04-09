12e0a925546a9bcb8d023ba7e56bc2fae90007bd
mario-lsb <pathakaditi96@gmail.com>
2026-04-09 14:32:40 +0530

    UC1 - Application Entry & Welcome Message

---
+++ App/src/UseCase1HotelBookingApp.java
@@ -0,0 +1,23 @@
+/**
+ * ============================================================
+ * MAIN CLASS - UseCase1HotelBookingApp
+ * ============================================================
+ *
+ * Use Case 1: Application Entry & Welcome Message
+ *
+ * Description:
+ * This class represents the entry point of the
+ * Hotel Booking Management System.
+ *
+ * @author Aditi
+ * @version 1.0
+ */
+
+public class UseCase1HotelBookingApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the Hotel Booking Management System");
+        System.out.println("System initialized successfully.");
+    }
+}
